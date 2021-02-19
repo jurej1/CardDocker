c993b61f1ee2f1b2602831c41254be8a82df7282
Jure Jures <jure.04jj@gmail.com>
2021-02-19 19:07:46 +0100

    repo

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -1,11 +1,19 @@
+import 'package:card_docker/blocs/blocs.dart';
 import 'package:card_docker/pages/login_page/view/login_view.dart';
+import 'package:card_docker/repositories/authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatelessWidget {
   static const routeName = 'login_page';
 
   @override
   Widget build(BuildContext context) {
-    return LoginView();
+    return BlocProvider(
+      create: (context) => LoginFormBloc(
+        firebaseAuthenticationRepository: RepositoryProvider.of<FirebaseAuthenticationRepository>(context),
+      ),
+      child: LoginView(),
+    );
   }
 }
