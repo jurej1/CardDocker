5547a18cd4338b750f0e87ca6507f48a21c06d4b
Jure Jures <jure.04jj@gmail.com>
2021-02-19 16:41:03 +0100

    initial pages created

---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:card_docker/app.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Center(
+        child: Text('HomePage'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page/register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register page'),
+      ),
+      body: Center(
+        child: Text('Register page'),
+      ),
+    );
+  }
+}
