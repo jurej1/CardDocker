773bda496cf681f08f1ca689245abfa50f4507db
Jure Jures <jure.04jj@gmail.com>
2021-02-21 17:27:37 +0100

    cards list

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:card_docker/blocs/auth_bloc/auth_bloc.dart';
+import 'package:card_docker/blocs/blocs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'widgets/widgets.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = 'home_page';
@@ -18,8 +21,22 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('HomePage'),
+      body: BlocBuilder<CardsBloc, CardsState>(
+        builder: (context, state) {
+          if (state is CardsLoading) {
+            return Center(
+              child: Text('Loading'),
+            );
+          } else if (state is CardsLoadFailure) {
+            return Center(
+              child: Text('Something went wrong'),
+            );
+          } else if (state is CardsLoadSuccess) {
+            return CardsList(cards: state.cards);
+          }
+
+          return Container();
+        },
       ),
     );
   }
---
+++ lib/pages/home_page/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'cards_list.dart';
