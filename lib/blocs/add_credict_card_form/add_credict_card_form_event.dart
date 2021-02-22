42fe49fd9e7222335f859daf702b48360cbcf669
Jure Jures <jure.04jj@gmail.com>
2021-02-22 20:41:32 +0100

    add card bloc stattes and events created

---
+++ lib/blocs/add_credict_card_form/add_credict_card_form_event.dart
@@ -0,0 +1,55 @@
+part of 'add_credict_card_form_bloc.dart';
+
+abstract class AddCredictCardFormEvent extends Equatable {
+  const AddCredictCardFormEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CredictCardCompanyChanged extends AddCredictCardFormEvent {
+  final CredictCardCompany company;
+
+  const CredictCardCompanyChanged(this.company);
+
+  @override
+  List<Object> get props => [props];
+}
+
+class CredictCardBalanceChanged extends AddCredictCardFormEvent {
+  final String value;
+
+  const CredictCardBalanceChanged(this.value);
+
+  @override
+  List<Object> get props => [value];
+}
+
+class CredictCardBalanceUnfocused extends AddCredictCardFormEvent {}
+
+class CredictCardTypeChanged extends AddCredictCardFormEvent {
+  final CredictCardType type;
+
+  const CredictCardTypeChanged(this.type);
+
+  @override
+  List<Object> get props => [type];
+}
+
+class CredictCardNoteChanged extends AddCredictCardFormEvent {
+  final String value;
+
+  const CredictCardNoteChanged(this.value);
+  @override
+  List<Object> get props => [value];
+}
+
+class CredictCardNoteUnfocused extends AddCredictCardFormEvent {}
+
+class CredictCardColorChanged extends AddCredictCardFormEvent {
+  final Color color;
+
+  const CredictCardColorChanged(this.color);
+  @override
+  List<Object> get props => [color];
+}
---
+++ lib/blocs/add_credict_card_form/add_credict_card_form_state.dart
@@ -0,0 +1,44 @@
+part of 'add_credict_card_form_bloc.dart';
+
+class AddCredictCardFormState extends Equatable {
+  const AddCredictCardFormState({
+    this.status,
+    this.company,
+    this.balance,
+    this.type,
+    this.note,
+    this.color,
+  });
+
+  final FormzStatus status;
+  final CredictCardCompany company;
+  final Balance balance;
+  final CredictCardType type;
+  final Note note;
+  final Color color;
+
+  @override
+  List<Object> get props => [status, company, balance, type, note, color];
+
+  @override
+  String toString() =>
+      'AddCredictCardFormState { status: $status, company: $company, balance: ${balance.value}, type: $type, note: $note, color: $color }';
+
+  AddCredictCardFormState copyWith({
+    FormzStatus status,
+    CredictCardCompany company,
+    Balance balance,
+    CredictCardType type,
+    Note note,
+    Color color,
+  }) {
+    return AddCredictCardFormState(
+      status: status ?? this.status,
+      company: company ?? this.company,
+      balance: balance ?? this.balance,
+      type: type ?? this.type,
+      note: note ?? this.note,
+      color: color ?? this.color,
+    );
+  }
+}
