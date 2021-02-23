67de57ea413818890adde7fddc7b444559f3bc70
Jure Jures <jure.04jj@gmail.com>
2021-02-23 20:36:37 +0100

    blocs

--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -2,3 +2,4 @@ export 'auth_bloc/auth_bloc.dart';
 export 'login_form/login_form_bloc.dart';
 export 'sign_up_form/sign_up_form_bloc.dart';
 export 'cards_bloc/cards_bloc.dart';
+export 'add_credict_card_form/add_credict_card_form_bloc.dart';
