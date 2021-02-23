29763b118484dd6fccbc968523d10f91a2ceee70
Jure Jures <jure.04jj@gmail.com>
2021-02-23 20:36:55 +0100

    credoct card page

--- lib/pages/add_credict_card/add_credict_card_page.dart
+++ lib/pages/add_credict_card/add_credict_card_page.dart
@@ -1,14 +1,21 @@
+import 'package:card_docker/blocs/add_credict_card_form/add_credict_card_form_bloc.dart';
+import 'package:card_docker/blocs/auth_bloc/auth_bloc.dart';
+import 'package:card_docker/pages/add_credict_card/view/view.dart';
+import 'package:card_docker/repositories/credict_cards_repository/credict_cards_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddCredictCardPage extends StatelessWidget {
   static const routeName = 'add_credict_card_page';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add credict card page'),
+    return BlocProvider<AddCredictCardFormBloc>(
+      create: (context) => AddCredictCardFormBloc(
+        authBloc: BlocProvider.of<AuthBloc>(context),
+        firebaseCredictCardRepository: RepositoryProvider.of<FirebaseCredictCardRepository>(context),
       ),
+      child: AddCredictCardView(),
     );
   }
 }
