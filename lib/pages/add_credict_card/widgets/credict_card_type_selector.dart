9c85c51bfe9c2ba8aa818703849f939a180aee97
Jure Jures <jure.04jj@gmail.com>
2021-02-24 07:32:17 +0100

    company and type selector modified to work with input card

--- lib/pages/add_credict_card/widgets/credict_card_type_selector.dart
+++ lib/pages/add_credict_card/widgets/credict_card_type_selector.dart
@@ -1,32 +1,32 @@
-import 'package:card_docker/blocs/blocs.dart';
-import 'package:card_docker/functions/credict_card_company_name.dart';
+import 'package:card_docker/blocs/add_credict_card_form/add_credict_card_form_bloc.dart';
+import 'package:card_docker/pages/add_credict_card/widgets/bottom_sheet_ground.dart';
 import 'package:card_docker/pages/add_credict_card/widgets/widgets.dart';
 import 'package:card_docker/repositories/credict_cards_repository/credict_cards_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CompanySelector extends StatelessWidget {
+class TypeSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AddCredictCardFormBloc, AddCredictCardFormState>(
       builder: (context, state) {
         return InputCard(
           onTap: () async {
-            final company = await showModalBottomSheet<CredictCardCompany>(
+            final type = await showModalBottomSheet<CredictCardType>(
               context: context,
               isScrollControlled: true,
               backgroundColor: Colors.transparent,
               barrierColor: Colors.black.withOpacity(0.6),
-              builder: (_) => _companiesBottomSheet(context, state.company),
+              builder: (_) => _typeSelectorBottomSheet(state.type),
             );
 
-            BlocProvider.of<AddCredictCardFormBloc>(context).add(CredictCardCompanyChanged(company));
+            BlocProvider.of<AddCredictCardFormBloc>(context).add(CredictCardTypeChanged(type));
           },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Company:'),
-              Text(credictCardCompanyName(state.company)),
+              Text('Type:'),
+              Text('Type'),
             ],
           ),
         );
@@ -34,20 +34,16 @@ class CompanySelector extends StatelessWidget {
     );
   }
 
-  Widget _companiesBottomSheet(BuildContext context, CredictCardCompany selectedCompany) {
+  Widget _typeSelectorBottomSheet(CredictCardType currentType) {
     return BottomSheetGround(
       child: ListView.builder(
         physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.all(20),
-        itemCount: CredictCardCompany.values.length,
+        itemCount: CredictCardType.values.length,
         itemBuilder: (context, index) {
-          final company = CredictCardCompany.values[index];
-
+          final type = CredictCardType.values[index];
           return ListTile(
-            selected: company == selectedCompany,
-            contentPadding: EdgeInsets.zero,
-            title: Text(credictCardCompanyName(company)),
-            onTap: () => Navigator.of(context).pop(company),
+            selected: type == currentType,
+            onTap: () => Navigator.of(context).pop(type),
           );
         },
       ),
