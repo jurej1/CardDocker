41945ca715a99cad171a606aeaf99af795f98b78
Jure Jures <jure.04jj@gmail.com>
2021-02-24 17:02:39 +0100

    entity

--- lib/repositories/transactions_repository/src/entities/transaction_entity.dart
+++ lib/repositories/transactions_repository/src/entities/transaction_entity.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 import 'package:card_docker/repositories/transactions_repository/src/enums/enums.dart';
+import 'package:flutter/foundation.dart';
 
 class TransactionEntity extends Equatable {
   final String id;
@@ -36,5 +37,58 @@ class TransactionEntity extends Equatable {
       connectedTo,
       created,
     ];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'ownerId': ownerId,
+      'note': note,
+      'title': title,
+      'purpose': describeEnum(purpose),
+      'amount': amount,
+      'connectedTo': connectedTo,
+      'created': created,
+    };
+  }
+
+  factory TransactionEntity.fromJson(Map<String, Object> data) {
+    return TransactionEntity(
+      amount: data['amount'],
+      connectedTo: data['connectedTo'],
+      created: data['created'],
+      id: data['id'],
+      note: data['note'],
+      ownerId: data['ownerId'],
+      title: data['title'],
+      purpose: stringToTransactionPurpose(data['purpose']),
+    );
+  }
+
+  factory TransactionEntity.fromDocument(DocumentSnapshot snap) {
+    final data = snap.data();
+
+    return TransactionEntity(
+      amount: data['amount'],
+      connectedTo: data['connectedTo'],
+      created: data['created'],
+      id: snap.id,
+      note: data['note'],
+      ownerId: data['ownerId'],
+      purpose: stringToTransactionPurpose(data['purpose']),
+      title: data['title'],
+    );
+  }
+
+   Map<String, dynamic> toDocument() {
+    return {
+      'ownerId': ownerId,
+      'note': note,
+      'title': title,
+      'purpose': describeEnum(purpose),
+      'amount': amount,
+      'connectedTo': connectedTo,
+      'created': created,
+    };
   }
 }
--- lib/repositories/transactions_repository/src/enums/transactions_purpose.dart
+++ lib/repositories/transactions_repository/src/enums/transactions_purpose.dart
@@ -9,7 +9,7 @@ enum TransactionPurpose {
   internet,
   carPayment,
   gasoline,
-  publicTransaportation,
+  publicTransportation,
   food,
   clothing,
   gym,
@@ -17,4 +17,47 @@ enum TransactionPurpose {
   homeDecor,
   alcohol,
   largePurchase,
+  none,
+}
+
+TransactionPurpose stringToTransactionPurpose(String value) {
+  if (value == 'rent') {
+    return TransactionPurpose.rent;
+  } else if (value == 'autoInsurance') {
+    return TransactionPurpose.autoInsurance;
+  } else if (value == 'healthInsurance') {
+    return TransactionPurpose.healthInsurance;
+  } else if (value == 'medicalCosts') {
+    return TransactionPurpose.medicalCosts;
+  } else if (value == 'electricity') {
+    return TransactionPurpose.electricity;
+  } else if (value == 'water') {
+    return TransactionPurpose.water;
+  } else if (value == 'sanitation') {
+    return TransactionPurpose.sanitation;
+  } else if (value == 'internet') {
+    return TransactionPurpose.internet;
+  } else if (value == 'carPayment') {
+    return TransactionPurpose.carPayment;
+  } else if (value == 'gasoline') {
+    return TransactionPurpose.gasoline;
+  } else if (value == 'publicTransportation') {
+    return TransactionPurpose.publicTransportation;
+  } else if (value == 'food') {
+    return TransactionPurpose.food;
+  } else if (value == 'clothing') {
+    return TransactionPurpose.clothing;
+  } else if (value == 'gym') {
+    return TransactionPurpose.gym;
+  } else if (value == 'movies') {
+    return TransactionPurpose.movies;
+  } else if (value == 'homeDecor') {
+    return TransactionPurpose.homeDecor;
+  } else if (value == 'alcohol') {
+    return TransactionPurpose.alcohol;
+  } else if (value == 'largePurchase') {
+    return TransactionPurpose.largePurchase;
+  } else {
+    return TransactionPurpose.none;
+  }
 }
