2a0a18c820724bacca7e5d88d8cf24bcab61a26f
Jure Jures <jure.04jj@gmail.com>
2021-03-01 15:52:25 +0100

    renaming

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,6 +2,6 @@ export 'email.dart';
 export 'password.dart';
 export 'display_name.dart';
 export 'note.dart';
-export 'balance.dart';
+export 'amount.dart';
 export 'card_id.dart';
 export 'title.dart';
