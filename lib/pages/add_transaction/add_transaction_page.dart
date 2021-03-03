8e1f0ea09a4d829f7539b0b89a0c614cab4b44f8
Jure Jures <jure.04jj@gmail.com>
2021-03-03 12:15:40 +0100

    add transaction page and view

--- lib/pages/add_transaction/add_transaction_page.dart
+++ lib/pages/add_transaction/add_transaction_page.dart
@@ -1,11 +1,21 @@
+import 'package:card_docker/blocs/add_transaction_form/add_transaction_form_bloc.dart';
+import 'package:card_docker/blocs/auth_bloc/auth_bloc.dart';
 import 'package:card_docker/pages/add_transaction/view.dart/add_transaction_view.dart';
+import 'package:card_docker/repositories/transactions_repository/src/firebase_transaction_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddTransactionPage extends StatelessWidget {
   static const routeName = 'add_transaction_page';
 
   @override
   Widget build(BuildContext context) {
-    return AddTransactionView();
+    return BlocProvider(
+      create: (context) => AddTransactionFormBloc(
+        authBloc: BlocProvider.of<AuthBloc>(context),
+        firebaseTransactionsRepository: RepositoryProvider.of<FirebaseTransactionsRepository>(context),
+      ),
+      child: AddTransactionView(),
+    );
   }
 }
--- lib/pages/add_transaction/view.dart/add_transaction_view.dart
+++ lib/pages/add_transaction/view.dart/add_transaction_view.dart
@@ -1,11 +1,49 @@
+import 'package:card_docker/blocs/add_transaction_form/add_transaction_form_bloc.dart';
+import 'package:card_docker/pages/add_transaction/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AddTransactionView extends StatelessWidget {
+class AddTransactionView extends StatefulWidget {
+  @override
+  _AddTransactionViewState createState() => _AddTransactionViewState();
+}
+
+class _AddTransactionViewState extends State<AddTransactionView> {
+  final FocusNode _titleFocusNode = FocusNode();
+  final FocusNode _amountFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _titleFocusNode.addListener(() {
+      if (!_titleFocusNode.hasFocus) BlocProvider.of<AddTransactionFormBloc>(context).add(TransactionTitleUnfocused());
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add transaction'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        physics: const BouncingScrollPhysics(),
+        children: [
+          TitleInput(focusNode: _titleFocusNode),
+          const SizedBox(height: 10),
+          NoteInput(),
+          const SizedBox(height: 10),
+          AmountInput(focusNode: _amountFocusNode),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
