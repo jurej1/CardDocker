6af72bd50ba0dabbc5502f7d3ab657a85d8f3eca
Jure Jures <jure.04jj@gmail.com>
2021-03-03 19:04:04 +0100

    clenaup

--- lib/pages/add_transaction/widgets/widgets.dart
+++ lib/pages/add_transaction/widgets/widgets.dart
@@ -1,6 +1,6 @@
 export 'note_input.dart';
 export 'title_input.dart';
-export 'transaction_type_input.dart';
+export 'transaction_purpose_input.dart';
 export 'amount_input.dart';
 export 'card_input.dart';
 export 'credict_card_tile.dart';
