e781852347f7ffff83427f324849aac5dfc54504
Jure Jures <jure.04jj@gmail.com>
2021-03-11 18:52:07 +0100

    stats view

--- lib/pages/home_page/view/stats_view.dart
+++ lib/pages/home_page/view/stats_view.dart
@@ -1,10 +1,32 @@
+import 'package:card_docker/blocs/blocs.dart';
+import 'package:card_docker/pages/home_page/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class StatsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Center'),
+    return BlocBuilder<StatsViewBloc, StatsViewState>(
+      builder: (context, state) {
+        if (state is StatsViewLoading) {
+          return const Center(
+            child: const CircularProgressIndicator(),
+          );
+        } else if (state is StatsViewLoadSuccess) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Stats'),
+            ),
+            body: StatsCardsList(),
+          );
+        } else if (state is StatsViewFailure) {
+          return Center(
+            child: Text('Oops something went wrong'),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
   }
 }
