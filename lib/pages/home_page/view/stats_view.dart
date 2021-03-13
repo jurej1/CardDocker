21312f8cb0f97c83ae6b7797ab095af49a2c0b21
Jure Jures <jure.04jj@gmail.com>
2021-03-13 20:26:04 +0100

    scaffold view

--- lib/pages/home_page/view/stats_view.dart
+++ lib/pages/home_page/view/stats_view.dart
@@ -6,28 +6,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class StatsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<StatsViewBloc, StatsViewState>(
-      builder: (context, state) {
-        if (state is StatsViewLoading) {
-          return const Center(
-            child: const CircularProgressIndicator(),
-          );
-        } else if (state is StatsViewLoadSuccess) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Stats'),
-            ),
-            body: StatsCardsList(),
-            bottomNavigationBar: const PageSelector(),
-          );
-        } else if (state is StatsViewFailure) {
-          return Center(
-            child: Text('Oops something went wrong'),
-          );
-        } else {
-          return Container();
-        }
-      },
+    return Scaffold(
+      bottomNavigationBar: const PageSelector(),
+      body: BlocBuilder<StatsViewBloc, StatsViewState>(
+        builder: (context, state) {
+          if (state is StatsViewLoading) {
+            return const Center(
+              child: const CircularProgressIndicator(),
+            );
+          } else if (state is StatsViewLoadSuccess) {
+            return StatsCardsList();
+          } else if (state is StatsViewFailure) {
+            return Center(
+              child: Text('Oops something went wrong'),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
     );
   }
 }
